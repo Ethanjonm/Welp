bc301a8004284da2c6548d62aef33f00eea87ace
Ethanjonm <ethanjonmercado@gmail.com>
2023-05-31 09:32:13 -0400

    backend phase 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,42 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  validates :email,
+            uniqueness: true,
+            presence: true,
+            length: { in: 3..255 },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { in: 6..255 }, allow_nil: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :fname, presence: true
+  validates :lname, presence: true
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(credential, password)
+    field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+    user = User.find_by(field => credential)
+    user&.authenticate(password)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
+
+  private
+
+  def generate_unique_session_token
+    loop do
+      token = SecureRandom.base64
+      break token unless User.exists?(session_token: token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'api/test', to: 'application#test'
+  post 'api/test', to: 'application#test'
 end
