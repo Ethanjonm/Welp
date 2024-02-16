7293f510a580694f7cdf1d4e21a8d21410c50e6b
Ethanjonm <ethanjonmercado@gmail.com>
2024-02-16 15:40:29 -0500

    import

---
+++ frontend/src/components/SplashPage/Cara.js
@@ -0,0 +1,7 @@
+import food1 from "../images/food1.jpg"
+import food2 from "../images/food2.jpg"
+import knifeburger from "../images/knifeburger.jpg"
+import food3 from "../images/food3.jpg"
+import food8 from "../images/food8.jpg"
+import food7 from "../images/food7.jpg"
+import food10 from "../images/food10.jpg"
